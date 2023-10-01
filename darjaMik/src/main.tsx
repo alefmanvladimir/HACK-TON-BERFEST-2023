28b550ca840d7db37ced7e53ebb2fecdfc3f66ac
dasha7879 <belka7879@gmail.com>
2023-10-01 22:50:22 +0300

    add TonConnectUIProvider

--- darjaMik/src/main.tsx
+++ darjaMik/src/main.tsx
@@ -1,12 +1,13 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 // this manifest is used temporarily for development purposes
-// const manifestUrl =
-//   "https://alefmanvladimir.github.io/my-twa/tonconnect-manifest.json";
-
+const manifestUrl = "https://alefmanvladimir.github.io/my-twa/tonconnect-manifest.json";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <TonConnectUIProvider manifestUrl={manifestUrl}>
     <App />
+  </TonConnectUIProvider>
 );
